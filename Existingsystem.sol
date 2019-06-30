049494245cfb8401ced0b6d6fb68c8d9700c32d9
Al Noman [帕维] <11007746+alnomanbd@users.noreply.github.com>
2019-07-01 02:57:11 +0800

    Update Existingsystem.sol

--- Existingsystem.sol
+++ Existingsystem.sol
@@ -7,7 +7,6 @@ import './strings.sol';
 contract FogBlockchainAuthentication{
             using strings for *;
 string public s; 
-event Log(string text);
     address      public  owner;
       struct Authentication {
         address requesteddeviceid;
@@ -15,17 +14,22 @@ event Log(string text);
         string previousblock;
         address accessdeviceid;
         string permissions;
+       // string location;
         uint timelease;
     }
 
     mapping (address => Authentication) fogdeviceauth;
+    //     string regname;
+    // address regdeviceid;  string regdeviceconfiguration;
+    // uint32 regmanufacturerid;string reglocations;
+    // string regipfsusercollagepicaddress;
       string ipfsusercollagepicaddress;
       
       struct FogDevice {
       string modelname;
         address deviceid;
         uint32 manufacturerid;
-        string locations;
+       // string locations;
         string usercollagepicaddress;
         string deviceconfiguration;
         int authcoins;
@@ -33,8 +37,14 @@ event Log(string text);
     }
     // Stored devices 
     mapping (address => FogDevice) fogdevices;
+  //  byte32 s4;
+ event Log(string text1,string text);
+ event Logs(string text1,uint32 text);
+ event Logint(string text1,int text);
 
- 
+ event Log(string text);
+ event Logs(uint32 text);
+ event Logint(int text);
     function sendImageHash(string ipfshash) public {
     ipfsusercollagepicaddress = ipfshash;
     }
@@ -44,9 +54,10 @@ event Log(string text);
  }
 
     
-    function register(string name,  string deviceconfiguration,uint32 manufacturerid,string locations,string ipfsusercollagepicaddress) 
+    function register(string name,  string deviceconfiguration,uint32 manufacturerid,string ipfsusercollagepicaddress) 
              public returns (bool res) {
         FogDevice dev = fogdevices[msg.sender];
+        // Return false if already registered.
         if ( dev.deviceid != 0x0) {
             return false;
         }
@@ -55,9 +66,12 @@ event Log(string text);
         dev.registertime = 100000;    
         dev.manufacturerid = manufacturerid;
         dev.deviceconfiguration=deviceconfiguration;
-        dev.usercollagepicaddress=getImageHash();
+        dev.usercollagepicaddress=ipfsusercollagepicaddress;
+        //dev.locations=locations;
+        dev.authcoins=100;
         fogdevices[msg.sender] = dev;
-        Log("Registered Successfully");
+//        s4=sha3(dev.modelname, dev.deviceid,dev.manufacturerid,dev.deviceconfiguration,dev.usercollagepicaddress);
+        Log(name,"Registered Successfully");
         return true;
     }
 
@@ -68,58 +82,56 @@ event Log(string text);
         dev.registertime = 0;
         dev.deviceconfiguration = "";
         dev.usercollagepicaddress="";
-         Log("UnRegistered Successfully");
+        dev.authcoins=0;
+         Log(dev.modelname,"UnRegistered Successfully");
         return true;
     }
 
-    function getDevInfo(address devid) public returns (string name, address deviceid,  string deviceconfiguration,uint32 manufacturerid,string locations,string ipfsusercollagepicaddress) {
+    function getDevInfo(address devid) public returns (string name, address deviceid,  string deviceconfiguration,uint32 manufacturerid,string ipfsusercollagepicaddress,int authcoins) {
         FogDevice dev = fogdevices[devid];
         name = dev.modelname;
         deviceid = dev.deviceid;
         deviceconfiguration = dev.deviceconfiguration;
         manufacturerid=dev.manufacturerid;
-        locations=dev.locations;
+        //locations=dev.locations;
         ipfsusercollagepicaddress=dev.usercollagepicaddress;
-         Log("Device Registered Successfully");
-        return(name,deviceid,deviceconfiguration,manufacturerid,locations,ipfsusercollagepicaddress);
+        authcoins=dev.authcoins;
+         Log(name,"Device Registered Successfully");
+        return(name,deviceid,deviceconfiguration,manufacturerid,ipfsusercollagepicaddress,authcoins);
     }
 
-    function getDevLocation(address devid) public returns (string locations) {
-        FogDevice dev = fogdevices[devid];
-        locations=dev.locations;
-        return(string(locations));
-    }
+    
 
 
     function remove() public returns (bool res) {
         if (msg.sender == owner) {
             suicide(owner);
-             Log("Smart contract suicide Successfully");
+             Log("Contract","Smart contract suicide Successfully");
         }
 
     }
   
-    string public fdlocation;
+    //string public fdlocation;
     
     
 
-    function checkAuthentication(address addr,string name, string deviceconfiguration,uint32 manufacturerid,string locations,string ipfsusercollagepicaddress, uint leasetime) 
+    function checkAuthentication(address addr,string name, string deviceconfiguration,uint32 manufacturerid,string ipfsusercollagepicaddress, uint leasetime) 
     public returns (bool res) {
-var(regname,regdeviceid,regdeviceconfiguration,regmanufacturerid,reglocations,regipfsusercollagepicaddress)= getDevInfo(addr);
- if ( keccak256(regname)==keccak256(name) && regmanufacturerid==manufacturerid &&  keccak256(regipfsusercollagepicaddress) == keccak256(ipfsusercollagepicaddress))
+var(regname,regdeviceid,regdeviceconfiguration,regmanufacturerid,regipfsusercollagepicaddress,regauthcoins)= getDevInfo(addr);
+ Log("regname ",regname);
+ Log("name",name);
+ Logs("regmanufacturerid",regmanufacturerid);
+ Logs("manufacturerid",manufacturerid);
+ Log("regipfsusercollagepicaddress",regipfsusercollagepicaddress);
+ Log("ipfsusercollagepicaddress",ipfsusercollagepicaddress);
+ Logint("regauthcoins",regauthcoins);
+ if ( keccak256(regname)==keccak256(name) && regmanufacturerid==manufacturerid &&  keccak256(regipfsusercollagepicaddress) == keccak256(ipfsusercollagepicaddress) && regauthcoins>1)
 {
 //     var s = reglocations.toSlice();
-    var spliinal=   reglocations.toSlice().find(locations.toSlice());
+   // var spliinal=   //reglocations.toSlice().find(locations.toSlice());
         
-        //bytes memory tempEmptyStringTest = bytes(spliinal.toString()); // Uses memory
-if (bytes(spliinal.toString()).length == 0) {
-    // emptyStringTest is an empty string
-} else {
-    
-             //bool success= RequestFogdevice(name,locations);
-             if(RequestFogdevice(name,locations)==true)
-             {
-                 giveAccesstoFD(addr,deviceconfiguration,manufacturerid,"Write",locations,leasetime);
+        //bytes memory tempEmptyStringTest = //bytes(spliinal.toString()); // Uses memory
+regauthcoins=regauthcoins-1;                 giveAccesstoFD(addr,deviceconfiguration,manufacturerid,"Write",leasetime);
               Log("Access granted Successfully");
                  return true;
              }
@@ -134,7 +146,7 @@ if (bytes(spliinal.toString()).length == 0) {
         //     bool success= RequestFogdevice(name,locations);
         //     if(success==true)
         //     {
-        //         giveAccesstoFD(addr,deviceconfiguration,manufacturerid,"Write",locations);
+        //         giveAccesstoFD(addr,deviceconfiguration,manufacturerid,"Write");
         //     }
           
         // }
@@ -142,45 +154,20 @@ if (bytes(spliinal.toString()).length == 0) {
 //feed.info.value(10).gas(800)(); 
 
 //string name,  string deviceconfiguration,uint32 manufacturerid,string locations,string ipfsusercollagepicaddress
+    
     Log("Access restricted");
+       regauthcoins=regauthcoins-50;
     return false;
     }
     
-    function RequestFogdevice(string FD, string location)  public returns (bool res) {
-    if( keccak256(getLocation()) == keccak256(location))
-    {
-         Log("Location FogDevice verified Successfully");
-        return true;
-    }
-    else
-      Log("Location FogDevice unverified Successfully");
-    return false;
-        
-    }
     
     
-function setLocation(string location, string ipfshash) public returns (bool)
-{
-     FogDevice dev1 = fogdevices[msg.sender];
-         if( keccak256(dev1.usercollagepicaddress) == keccak256(ipfshash)){
-          s = dev1.locations.toSlice().concat(location.toSlice());
-         dev1.locations=s;
-        Log("Location added Successfully to Authorisation list");
-         }
-
-}
-
-function getLocation() public returns (string)
-{
-    return fdlocation;
-}
-
+    
 
 function giveAccesstoFD(address requesteddeviceid,
         string deviceinfo,
         address accessdeviceid,
-        string permissions,
-        string location, uint leaseTime) expire(msg.sender) public returns (bool)
+        string permissions, uint leaseTime) expire(msg.sender) public returns (bool)
 {
    
      Authentication devauth = fogdeviceauth[msg.sender];
@@ -189,6 +176,7 @@ function giveAccesstoFD(address requesteddeviceid,
         devauth.deviceinfo=deviceinfo;
         devauth.accessdeviceid=accessdeviceid;
         devauth.permissions=permissions;
+        devauth.timelease=block.timestamp + leaseTime;
         fogdeviceauth[msg.sender] = devauth;
         return true;
 }
@@ -198,12 +186,13 @@ function giveAccesstoFD(address requesteddeviceid,
         string deviceinfo,
         address accessdeviceid,
         string permissions,
-        string location, uint leaseTime) {
+         uint leaseTime) {
         Authentication devauth = fogdeviceauth[msg.sender];
         requesteddeviceid= devauth.requesteddeviceid;
         deviceinfo=devauth.deviceinfo;
         accessdeviceid=devauth.accessdeviceid;
         permissions=devauth.permissions;
+        //location=devauth.location;
         leaseTime=devauth.timelease-now;
     }
 
@@ -221,6 +210,7 @@ function autoDisconnect() public returns (bool)
         devauth.previousblock="";
         devauth.accessdeviceid=0x0;
         devauth.permissions="";
+       // devauth.location="";
         devauth.timelease=0;
         }
         _;
@@ -235,12 +225,13 @@ function removeAccesstoFD() public returns (bool)
         devauth.previousblock="";
         devauth.accessdeviceid=0x0;
         devauth.permissions="";
+       // devauth.location="";
         devauth.timelease=0;
         fogdeviceauth[msg.sender] = devauth;
         return true;
 }
 
    
-
     
 }
+
